4506eb1de2a232ef1e56ee440e4621203ffac797
Antoninadan <mail100@i.ua>
2019-11-16 05:15:03 +0200

    initial commit

---
+++ src/main/java/com/mainacad/model/Item.java
@@ -0,0 +1,20 @@
+package com.mainacad.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Item {
+    private String itemId;
+    private String name;
+    private String url;
+    private String imageUrl;
+    private BigDecimal price;
+}
---
+++ src/main/java/com/mainacad/service/OlxProductParserService.java
@@ -0,0 +1,94 @@
+package com.mainacad.service;
+
+import com.mainacad.model.Item;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class OlxProductParserService extends Thread {
+    private static final Logger LOG = Logger.getLogger(OlxProductParserService.class.getName());
+
+    private final List<Item> items;
+    private final String url;
+
+    public OlxProductParserService(List<Item> items, String url) {
+        this.items = items;
+        this.url = url;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Document document = Jsoup.connect(url).get();
+//            Element productInfo = document.getElementsByClass("offer-titlebox").first();
+            String itemId = extractItemId(document);
+            String name = extractName(document);
+            BigDecimal price = extractPrice(document);
+            String imageUrl = extractImageUrl(document);
+
+            Item item = new Item(itemId, name, url, imageUrl, price);
+            items.add(item);
+        } catch (IOException e) {
+            LOG.severe(String.format("Item by URL %s was not extracted", url));
+        }
+    }
+
+    private String extractItemId(Document document) {
+        String result = "";
+        try {
+            Element productInfo = document.getElementsByClass("offer-titlebox").first();
+            result = productInfo.getElementsByTag("SMALL").first().text().replaceAll("\\D+","");
+            return result;
+        } catch (Exception e) {
+            LOG.severe(String.format("Item id by URL %s was not extracted", url));
+        }
+        return result;
+    }
+
+    private String extractName(Document document) {
+        String result = "";
+        try {
+            Element productInfo = document.getElementsByClass("offer-titlebox").first();
+            result = productInfo.getElementsByTag("H1").first().text();
+            return result;
+        } catch (Exception e) {
+            LOG.severe(String.format("Item product name by URL %s was not extracted", url));
+        }
+        return result;
+    }
+
+    private BigDecimal extractPrice(Document document) {
+        BigDecimal result = null;
+        try {
+
+            Element product = document.getElementsByClass("xxxx-large not-arranged").first();
+            String resultAsText = product.text();
+            resultAsText = resultAsText.replaceAll("[^0-9,]", "");
+            resultAsText = resultAsText.replaceAll("[,]", ".");
+            result = new BigDecimal(resultAsText).setScale(2, RoundingMode.HALF_UP);
+        } catch (Exception e) {
+            LOG.severe(String.format("Item price by URL %s was not extracted", url));
+        }
+        return result;
+    }
+
+
+    private String extractImageUrl(Element productInfo) {
+        String result = "";
+        try {
+            result = productInfo.getElementsByAttributeValue("property", "og:image").first().attr("content");
+            return result;
+        } catch (Exception e) {
+            LOG.severe(String.format("Item image url by URL %s was not extracted", url));
+        }
+        return result;
+    }
+
+
+}
